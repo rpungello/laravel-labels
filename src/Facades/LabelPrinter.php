c9b78835bfd154a1066880e8504b8bd05c592d3d
Rob Pungello <rob.pungello@gmail.com>
2022-07-14 13:59:46 -0400

    Fix facade

--- src/Facades/LabelPrinter.php
+++ src/Facades/LabelPrinter.php
@@ -4,6 +4,9 @@ namespace Rpungello\LaravelLabels\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see \Rpungello\LaravelLabels\LabelPrinter
+ */
 class LabelPrinter extends Facade
 {
     protected static function getFacadeAccessor(): string

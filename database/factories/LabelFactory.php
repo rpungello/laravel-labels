5c1447d92adbe664815b0c6d48d0b1d38ca289ea
Rob Pungello <rob.pungello@gmail.com>
2022-07-13 20:38:59 -0400

    Use older faker access

--- database/factories/LabelFactory.php
+++ database/factories/LabelFactory.php
@@ -12,16 +12,14 @@ class LabelFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->word(),
-            'page_width' => fake()->numberBetween(100, 300),
-            'page_height' => fake()->numberBetween(100, 300),
-            'label_height' => fake()->numberBetween(20, 40),
-            'label_width' => fake()->numberBetween(40, 80),
-            'horizontal_spacing' => fake()->randomNumber(1),
-            'vertical_spacing' => fake()->randomNumber(1),
-            'padding' => fake()->randomNumber(1),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'name' => $this->faker->word(),
+            'page_width' => $this->faker->numberBetween(100, 300),
+            'page_height' => $this->faker->numberBetween(100, 300),
+            'label_height' => $this->faker->numberBetween(20, 40),
+            'label_width' => $this->faker->numberBetween(40, 80),
+            'horizontal_spacing' => $this->faker->randomNumber(1),
+            'vertical_spacing' => $this->faker->randomNumber(1),
+            'padding' => $this->faker->randomNumber(1),
         ];
     }
 }

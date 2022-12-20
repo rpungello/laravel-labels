02b326d5cedebf0d6c6ee06690eeb6284a44bba7
Rob Pungello <rob.pungello@gmail.com>
2022-12-20 09:27:16 -0500

    Clean up code

--- tests/LabelTest.php
+++ tests/LabelTest.php
@@ -1,59 +1,68 @@
 <?php
 
+use Rpungello\LaravelLabels\ArrayLabel;
+use Rpungello\LaravelLabels\LabelPrinter;
+use function Pest\Laravel\assertModelExists;
+use function PHPUnit\Framework\assertEquals;
+
+use Rpungello\LaravelLabels\Models\Label;
+
 it('can create a model', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create();
-    \Pest\Laravel\assertModelExists($label);
+    $label = Label::factory()->create();
+    assertModelExists($label);
 });
 
 it('can calculate effective page width', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create([
+    $label = Label::factory()->create([
         'page_width' => 215.9,
         'horizontal_margin' => 5,
     ]);
 
-    \PHPUnit\Framework\assertEquals(205.9, $label->getEffectivePageWidth());
+    assertEquals(205.9, $label->getEffectivePageWidth());
 });
 
 it('can calculate effective page height', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create([
+    $label = Label::factory()->create([
         'page_height' => 279.4,
         'vertical_margin' => 5,
     ]);
 
-    \PHPUnit\Framework\assertEquals(269.4, $label->getEffectivePageHeight());
+    assertEquals(269.4, $label->getEffectivePageHeight());
 });
 
 it('can calculate number of columns', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create([
+    $label = Label::factory()->create([
         'page_width' => 215.9,
         'horizontal_margin' => 5,
         'label_width' => 100,
         'horizontal_spacing' => 5,
     ]);
 
-    \PHPUnit\Framework\assertEquals(2, $label->getNumberOfColumns());
+    assertEquals(2, $label->getNumberOfColumns());
 });
 
 it('can calculate number of rows', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create([
+    $label = Label::factory()->create([
         'page_height' => 279.4,
         'vertical_margin' => 5,
         'label_height' => 50,
         'vertical_spacing' => 5,
     ]);
 
-    \PHPUnit\Framework\assertEquals(4, $label->getNumberOfRows());
+    assertEquals(4, $label->getNumberOfRows());
 });
 
 it('can avoid rounding errors', function () {
-    $label = \Rpungello\LaravelLabels\Models\Label::factory()->create([
+    $label = Label::factory()->create([
         'page_height' => 279.4,
         'vertical_margin' => 12.7,
         'label_height' => 25.4,
         'vertical_spacing' => 0,
     ]);
 
-    \PHPUnit\Framework\assertEquals(10, $label->getNumberOfRows());
+    assertEquals(10, $label->getNumberOfRows());
+});
+
 it('prints the correct number of pages', function () {
     $printer = new LabelPrinter();
     $template = Label::factory()->create([
@@ -81,4 +90,6 @@ it('prints the correct number of pages', function () {
         ]);
     }
 
+    $pdf = $printer->getPdfFromArray($template, $labels);
+    assertEquals($pdf->getNumPages(), 3);
 });
